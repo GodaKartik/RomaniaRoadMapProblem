8b1cfa58235126eb712a7d74a73569e71afd9d5b
GodaKartik <90519209+GodaKartik@users.noreply.github.com>
2022-09-20 15:19:53 +0530

    Create greedy_best_first_search.py

---
+++ greedy_best_first_search.py
@@ -0,0 +1,19 @@
+from data import roadmap_as_graph, heuristic_values
+
+def GreedyBestFirstSearch(graph, heuristics, start, target):
+    path = [start] 
+    next_child = start
+    while next_child != target:
+        child_heuristics = []
+        sorted_children = []
+        for city in graph[next_child]:
+            h_val = heuristic_values[city]
+            child_heuristics.append(h_val)
+        sorted_child_heuristics = sorted(child_heuristics)
+        lowest_h_val = sorted_child_heuristics[0]
+        next_child = list(heuristics.keys())[list(heuristics.values()).index(lowest_h_val)]
+        path.append(next_child)
+    return path
+  
+  # using Arad to Bucharest as an example
+  GreedyBestFirstSearch(roadmap_as_graph, heuristic_values, "Arad", "Bucharest")
